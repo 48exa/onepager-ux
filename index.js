fa20fb0c9b24cbf16ca2a654f266b8c6e859749e
48exa <douwewesterdijk@gmail.com>
2022-12-14 12:32:22 +0100

    Cookie popup
    Moet nog functionaliteit toevoegen, de knopper verwijderen nu de cookie popup.

---
+++ index.js
@@ -0,0 +1,7 @@
+const BG_BLUR = document.querySelector(".background-blur")
+const COOKIE_POPUP = document.querySelector(".cookies-popup")
+const COOKIE_H3 = document.querySelector(".cookies-popup h3")
+
+function cookieCheck() {
+    BG_BLUR.style.display = "none";
+}
