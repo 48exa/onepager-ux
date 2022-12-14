485b3f019ea39b0e0a3479d590220b5ce871770b
48exa <douwewesterdijk@gmail.com>
2022-12-14 19:45:19 +0100

    Finished the cookies popup

---
+++ script/cookiescreen.js
@@ -0,0 +1,43 @@
+const BG_BLUR = document.querySelector(".background-blur"),
+    COOKIE_POPUP = document.querySelector(".cookies-popup"),
+    COOKIE_H3 = document.querySelector(".cookies-popup h3"),
+    COOKIE_P = document.querySelector(".cookies-popup p")
+
+let cookies = "not set"
+
+function clear() {
+    localStorage.clear()
+}
+
+function cookieAccept() {
+    cookies = "working"
+    localStorage.setItem('value', cookies)
+    cookieCheck()
+}
+
+function cookieReject() {
+    cookies = "not working"
+    localStorage.setItem('value', cookies)
+    cookieCheck()
+}
+
+function cookieCheck() {
+    if (localStorage.getItem('value') == "not working") {
+        COOKIE_H3.innerHTML = "We don't value your privacy."
+        COOKIE_P.innerHTML = "Accept the cookies."
+        COOKIE_POPUP.style.height = "175px"
+    } else if (localStorage.getItem('value') == "working") {
+        //BG_BLUR.style.display = "none"
+        COOKIE_POPUP.style.display = "none"
+    }
+}
+
+if (localStorage.getItem('value') == "not working") {
+    COOKIE_H3.innerHTML = "We don't value your privacy."
+    COOKIE_P.innerHTML = "Accept the cookies."
+    COOKIE_POPUP.style.height = "175px"
+} else if (localStorage.getItem('value') == "working") {
+    //BG_BLUR.style.display = "none"
+    COOKIE_POPUP.style.display = "none"
+}
+
